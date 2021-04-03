d29030c85679aec0583417b31deee82eb9f90316
hw-star <3111314916@qq.com>
2021-04-03 20:19:15 +0800

    修复bug

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,8 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/admin/',
+  // publicPath: '/admin/',
+  publicPath: '/',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: false, //process.env.NODE_ENV === 'development'
